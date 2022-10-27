78b4e735f59d9a13fb8b7bc0ca2324c84763a976
n8raid <denizbayram420@gmail.com>
2022-10-27 16:34:29 +0300

    ikili haberlesme

--- nano.ino
+++ nano.ino
@@ -1,32 +1,32 @@
-/*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(10, 9); // CE, CSN
 
 const byte address[6] = "00001";
 
+// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+struct Data_Package {
+  byte a = 0;
+  byte b = 125;
+  byte c = 255;
+  int d = 1024;
+  float e = 3.141592;
+  String f = "Test";
+};
+
+Data_Package data; // Create a variable with the above structure
+
 void setup() {
-  Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.stopListening();
 }
 
 void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+  // Send the whole data from the structure to the receiver
+  radio.write(&data, sizeof(Data_Package));
+  delay(1500);
 }
